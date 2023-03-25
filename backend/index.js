2ac559b3d0ffd711b3621eba409fcf95468dc416
Kaifur Rahman <rahmankaifur8@gmail.com>
2023-03-25 13:51:46 +0530

    backend to frontend transferred

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,15 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import { initializeApp } from 'firebase/app';
 import { getDatabase , ref, onValue} from "firebase/database";
+import cors from "cors";
 
 
 
 //app configuration
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = 5000;
+app.use(cors());
 
 //db connect
 mongoose.set("strictQuery", false);
@@ -34,13 +35,18 @@ mongoose
   const firebaseConfig = {
     databaseURL: "https://nirbhaya-f298b-default-rtdb.firebaseio.com/",
   };
-
+//firebase db initializing
   const app2 = initializeApp(firebaseConfig);
 
+//getting location of user 1 real time
+var latitude=0;
+var longitude=0;
   const db = getDatabase();
   const starCountRef = ref(db, 'Locations/user1');
   onValue(starCountRef, (snapshot) => {
     const data = snapshot.val();
+    latitude=data.latitude;
+    longitude=data.longitude;
     console.log(data);
   });
   
@@ -56,8 +62,10 @@ import womenRoutes from "./views/women.js";
 //middlewares
 app.use("/women", womenRoutes);
 
-  
-
+//sso triggered sample
+app.get('/sso',function(req,res){
+  res.json({lat:latitude,long:longitude})
+});
 //port
 app.listen(PORT, function () {
     console.log("Server started on port: " + PORT);
